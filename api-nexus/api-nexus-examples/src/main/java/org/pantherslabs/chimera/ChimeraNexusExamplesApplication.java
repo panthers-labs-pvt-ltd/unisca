33cf6ac7c8cc1914fac6502a13121b3e07ab374f
Manish <offical.kumar.manish@gmail.com>
2025-06-26 14:11:53 +0000

    Correct Folder Structure and Group Names

--- api-nexus/api-nexus-examples/src/main/java/org/pantherslabs/chimera/ChimeraNexusExamplesApplication.java
+++ api-nexus/api-nexus-examples/src/main/java/org/pantherslabs/chimera/ChimeraNexusExamplesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.progressive.minds.chimera.examples")
-@MapperScan("com.progressive.minds.chimera.examples.mapper.generated")
+@ComponentScan(basePackages = ".pantherslabs.chimera.examples")
+@MapperScan(".pantherslabs.chimera.examples.mapper.generated")
 public class ChimeraNexusExamplesApplication {
   public static void main(String[] args) {
     SpringApplication.run(ChimeraNexusExamplesApplication.class, args);
