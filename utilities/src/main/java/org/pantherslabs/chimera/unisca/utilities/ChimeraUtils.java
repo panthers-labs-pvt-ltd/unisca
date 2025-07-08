bffde34602fcf7cf73c89f0764e0cc60981b7db7
Manish <offical.kumar.manish@gmail.com>
2025-07-08 17:32:50 +0000

    Added New Functions in Utility

--- utilities/src/main/java/org/pantherslabs/chimera/unisca/utilities/ChimeraUtils.java
+++ utilities/src/main/java/org/pantherslabs/chimera/unisca/utilities/ChimeraUtils.java
@@ -1,12 +1,24 @@
 package org.pantherslabs.chimera.unisca.utilities;
 
-import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
 
 public class ChimeraUtils {
 
@@ -24,7 +36,7 @@ public class ChimeraUtils {
             JsonGenerator generator = mapper.createGenerator(baos, JsonEncoding.UTF8);
             block.write(generator);
             generator.close();
-            return new String(baos.toByteArray(), StandardCharsets.UTF_8);
+            return baos.toString(StandardCharsets.UTF_8);
         }
     }
 
@@ -43,5 +55,64 @@ public class ChimeraUtils {
     @FunctionalInterface
     public interface JsonGeneratorBlock {
         void write(JsonGenerator generator) throws IOException;
+    }
+
+    public static String convertTimeFormat(String timeFormat, Calendar calendar) {
+        SimpleDateFormat format = new SimpleDateFormat(timeFormat);
+        format.setTimeZone(TimeZone.getTimeZone("Europe/London"));
+        return format.format(calendar.getTime());
+    }
+
+    public static  Calendar currentGMTCalendar() {
+        TimeZone timeZone = TimeZone.getTimeZone("UTC");
+        return Calendar.getInstance(timeZone);
+    }
+
+    public static boolean isValidJson(String jsonString) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.readTree(jsonString);
+            return true;
+        } catch (Throwable e) {
+            return false;
+        }
+    }
+
+    public static Map<String, String> jsonToMapConverter(String inJsonConf) {
+        Map<String, String> map = new HashMap<>();
+        if (inJsonConf != null && !inJsonConf.isBlank()) {
+            List<HashMap<String, String>> userConf = new Gson().fromJson(inJsonConf,
+                    new TypeToken<List<HashMap<String, String>>>() {}.getType());
+            for (HashMap<String, String> item : userConf) {
+                map.put(item.get("Key"), item.get("Value"));
+            }
+        }
+        return map;
+    }
+    public static String transformMetadataFields(String elementValue, JsonNode eventJSON) {
+        String returnValue = "";
+        if (!isNullOrBlank(elementValue)) {
+            returnValue = elementValue;
+            Pattern stringPattern = Pattern.compile("\\$\\{trigger/(.*?)}");
+            if (elementValue.contains("${trigger/")) {
+                Matcher patternMatches = stringPattern.matcher(elementValue);
+                while (patternMatches.find()) {
+                    String curMatch = patternMatches.group();
+                    String replacingString = eventJSON.at(curMatch.substring(9, curMatch.length() - 1)).toString()
+                            .replace("\"", "");
+                    returnValue = returnValue.replace(curMatch, replacingString);
+                }
+            }
+        }
+        return returnValue;
+    }
+
+    public static boolean isNullOrBlank(String i) {
+        return i == null || i.isEmpty();
+    }
+
+    @FunctionalInterface
+    public interface JsonGeneratorConsumer {
+        void accept(JsonGenerator generator) throws Exception;
     }
 }
