9639d94c150a150f531897c30137678f024c47b2
Manish <offical.kumar.manish@gmail.com>
2025-07-10 17:23:24 +0000

    Modified Configuration Execution Engine

--- execution_engine/src/test/java/SparkSessionTest.java
+++ execution_engine/src/test/java/SparkSessionTest.java
@@ -9,7 +9,7 @@ public class SparkSessionTest {
         OptimizedSparkSession spark = OptimizedSparkSession.get("DataQuality","test");
         String isHiveEnabled = spark.conf().get("spark.sql.catalogImplementation");
         System.out.println(isHiveEnabled);
-        spark.sql("CREATE TABLE TEST (name String)");
+        spark.sql("CREATE TABLE IF NOT EXISTS TEST (name String)");
         //spark.sql("CREATE TABLE test_hive_table AS SELECT 1 AS id");
         spark.sql("SHOW TABLES").show();
     }
